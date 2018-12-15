d058b7e3362d342b3d8f8648e575c33022351d8a
Gabriel Macario <gmacarioweb@gmail.com>
2018-12-15 15:21:42 -0800

    Fixed testing environment with Enzyme in App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,13 @@
 /* global it, expect */
 
 import React from 'react';
-import { shallow } from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const component = Enzyme.shallow(<App />);
   expect(component.exists()).toEqual(true);
 });
